5fcdbad90adcc95cd920357cc7a4872164bcad1b
devang <vadherdevang0@gmail.com>
2024-03-29 13:53:02 +0530

    Add project files.

---
+++ MovieRatingWeb/Controllers/MovieController.cs
@@ -0,0 +1,249 @@
+﻿using MovieRatingWeb.Data;
+using MovieRatingWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieRatingWeb.Controllers
+{
+    public class MovieController : Controller
+    {
+        private dbcontext s2;
+        private object userService;
+
+        public MovieController(dbcontext dbcontext)
+        {
+            s2 = dbcontext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        public IActionResult TvSeries()
+        {
+            return View();
+        }
+
+        public IActionResult Trending()
+        {
+            return View();
+        }
+
+        public IActionResult TopRated()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SignUp(UserModel abc)
+        {
+            s2.Users.Add(abc);
+            s2.SaveChanges();
+
+            return RedirectToAction("Home");
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(UserModel abc)
+        {
+            var user = s2.Users.FirstOrDefault(u => u.UserEmail == abc.UserEmail && u.UserPassword == abc.UserPassword);
+            if (user != null)
+            {
+                HttpContext.Session.SetString("UserSession", user.UserEmail);
+                return RedirectToAction("Home");
+            }
+            else
+            {
+                ViewBag.Message = "Login Failed !!!";
+            }
+            return View(abc);
+        }
+
+        //public async Task<IActionResult> ShowMore()
+        //{
+        //    var comments = await s2.Comments.Include(c => c.User).ToListAsync();
+
+        //    if (HttpContext.Session.GetString("UserSession") != null)
+        //    {
+        //        ViewBag.MySession = HttpContext.Session.GetString("UserSession").ToString();
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction("Login");
+        //    }
+        //    return View(comments);
+        //}
+
+        // MovieController.cs
+
+        public async Task<IActionResult> ShowMore()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                ViewBag.MySession = HttpContext.Session.GetString("UserSession").ToString();
+                var comments = await s2.Comments.Include(c => c.User).ToListAsync();
+                return View(comments);
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+
+        public IActionResult ShowMore2()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                ViewBag.MySession = HttpContext.Session.GetString("UserSession").ToString();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        public IActionResult ShowMore3()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                ViewBag.MySession = HttpContext.Session.GetString("UserSession").ToString();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        public IActionResult ShowMore4()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                ViewBag.MySession = HttpContext.Session.GetString("UserSession").ToString();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        public IActionResult ShowMore5()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                ViewBag.MySession = HttpContext.Session.GetString("UserSession").ToString();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        public IActionResult ShowMore6()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                ViewBag.MySession = HttpContext.Session.GetString("UserSession").ToString();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        public IActionResult ShowMore7()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                ViewBag.MySession = HttpContext.Session.GetString("UserSession").ToString();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        public IActionResult ShowMore8()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                ViewBag.MySession = HttpContext.Session.GetString("UserSession").ToString();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                HttpContext.Session.Remove("UserSession");
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddComment(CommentModel comment)
+        {
+           
+             
+                    var userEmail = HttpContext.Session.GetString("UserSession");
+                    var user = await s2.Users.FirstOrDefaultAsync(u => u.UserEmail == userEmail);
+
+                    if (user != null)
+                    {
+                        comment.Text = comment.Text;
+                        comment.UserId = user.UserId;
+                        comment.PostedAt = DateTime.Now;
+
+                        s2.Comments.Add(comment);
+                        await s2.SaveChangesAsync();
+
+                        return RedirectToAction("ShowMore");
+                    }
+                
+
+                var comments = await s2.Comments.Include(c => c.User).ToListAsync();
+                return View("ShowMore", comments);
+            
+        }
+
+    }
+}
---
+++ MovieRatingWeb/Migrations/20240325190750_Comments.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieRatingWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class Comments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    CommentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.CommentId);
+                    table.ForeignKey(
+                        name: "FK_Comments_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_UserId",
+                table: "Comments",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+        }
+    }
+}
